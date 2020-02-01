0fd4c5d4dd1e8418edb424e7c8cb0cadafa2fdc0
Teemu Suvanto <extern.suvanto_teemu@allianz.de>
2020-02-01 20:15:56 +0100

    hide

--- az.tf
+++ az.tf
@@ -1,7 +1,18 @@
+
+variable "subscription_id" { type = string }
+variable "client_id"       { type = string }
+variable "client_secret"   { type = string }
+variable "tenant_id"       { type = string }
+
 # Configure the Azure Provider
 provider "azurerm" {
   # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
   version = "=1.42.0"
+  
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
 # Create a resource group
