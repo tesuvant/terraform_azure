94396bd787bc0a5886c98a39aa20c1a01b2eed58
Teemu Suvanto <teemu.suvanto@gmail.com>
2020-04-09 14:58:25 +0200

    final?

--- az.tf
+++ az.tf
@@ -176,7 +176,7 @@ resource "azurerm_virtual_machine" "myterraformvm" {
       ]
 
       connection {
-        host        = azurerm_public_ip.myterraformpublicip.properties.ipAddress
+        host        = azurerm_public_ip.myterraformpublicip.*.ipAddress
         type        = "ssh"
         password    = var.pw
         user        = "azureuser"
