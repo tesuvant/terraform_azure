785de51a2a88bd87f9cfd15f77c5297213a93a71
Teemu Suvanto <teemu.suvanto@gmail.com>
2020-04-09 15:33:35 +0200

    final?

--- az.tf
+++ az.tf
@@ -125,6 +125,7 @@ resource "azurerm_storage_account" "mystorageaccount" {
 
 
 resource "azurerm_managed_disk" "osdisk" {
+  name                 = "md"
   location             = "westeurope"
   storage_account_type = "Standard_LRS"
   create_option        = "Empty"
