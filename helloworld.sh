ae03ce647b57fa12e3ef3110f98c1ea3beb35fd3
Teemu Suvanto <teemu.suvanto@gmail.com>
2020-04-13 17:51:53 +0200

    azurerm_virtual_machine_extension

--- helloworld.sh
+++ helloworld.sh
@@ -1,2 +1,11 @@
 #!/bin/bash -eux
-touch /tmp/helloworld.txt
+
+#sudo /bin/bash -l -c '
+cat << EOF >> /etc/bash.bashrc
+export HTTP_PROXY=http://myproxy.foo.bar:3128
+export HTTPS_PROXY=http://myproxy.foo.bar:3128
+export NO_PROXY=.domain.com,.domain.org
+export http_proxy=http://myproxy.foo.bar:3128
+export https_proxy=http://myproxy.foo.bar:3128
+export no_proxy=.domain.com,.domain.org
+EOF
