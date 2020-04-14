9dff5632bcdfac5aef09284d4129c5b09cb7748b
Teemu Suvanto <teemu.suvanto@gmail.com>
2020-04-14 17:15:59 +0200

    idea

--- az.tf
+++ az.tf
@@ -247,7 +247,7 @@ SCRIPT
 
 resource "azurerm_virtual_machine_extension" "proxy" {
   name                 = "proxy"
-  virtual_machine_id   = azurerm_linux_virtual_machine.main.id
+  virtual_machine_id   = azurerm_linux_virtual_machine.myterraformvm.id
   publisher            = "Microsoft.Azure.Extensions"
   type                 = "CustomScript"
   type_handler_version = "2.0"
