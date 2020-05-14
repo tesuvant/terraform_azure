ce451700f54ccf358c112ccaf109fd928e5c9fef
Teemu Suvanto <teemu.suvanto@gmail.com>
2020-05-14 13:51:25 +0200

    edit

--- az.tf
+++ az.tf
@@ -14,9 +14,9 @@ provider "dns" {
 
 output "inconsult_addrs" {
 #  value = "${join(",", data.dns_a_record_set.allow_hosts.addrs)}"
-  #value = data.dns_a_record_set.allow_hosts[*].addrs
+  value = data.dns_a_record_set.allow_hosts.*.addrs
   #value = tolist(data.dns_a_record_set.allow_hosts[*])     # returns: addrs, host, id
-  value = "${join(",", [data.dns_a_record_set.allow_hosts[*].addrs])}"
+  #value = "${join(",", data.dns_a_record_set.allow_hosts.*.addrs)}"
 }
 #   "outputs": {
 #     "inconsult_addrs": {
